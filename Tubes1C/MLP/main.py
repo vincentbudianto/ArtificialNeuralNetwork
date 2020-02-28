133cc465e60a4342bc5333a250c3c2e259204ac6
Meyjan <saragih.meyer0821@gmail.com>
2020-02-28 15:49:39 +0700

    Set divergineMaxCount to 10

--- Tubes1C/MLP/main.py
+++ Tubes1C/MLP/main.py
@@ -69,7 +69,7 @@ def main():
             return [1, 1, 1]
 
     model = generateModel(0.05)
-    model.learn(dataDict, dataSplitCount, nodeOutputCheck, maxIteration=1000, minError=1, divergingMaxCount=20)
+    model.learn(dataDict, dataSplitCount, nodeOutputCheck, maxIteration=1000, minError=1, divergingMaxCount=10)
     model.predict(predictData, nodeOutputCheck)
 
     # Test result
